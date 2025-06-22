35c923a48ce0e2af14d81b6ca2cf50306ca8ba7c
SAGAR KUMAR DAS <108587278+Sagarkumardas2002@users.noreply.github.com>
2025-06-22 13:41:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -1,38 +1,45 @@
+
 class Solution {
     typedef pair<int,int>P;
   public:
     int spanningTree(int V, vector<vector<int>> adj[]) {
         
-          priority_queue<P, vector<P>, greater<P>> pq;
+        // Min-heap to always pick the edge with the smallest weight
+        priority_queue<P, vector<P>, greater<P>> pq;
         
-        pq.push({0,0});
-        vector<bool>inMST(V,false);
-        int sum=0;
+        pq.push({0,0});// Start with node 0 and weight 0
+
+        vector<bool>inMST(V,false); // Track nodes already included in MST
+        
+        int totalWeight =0; // Store the total weight of MST
         
         while(!pq.empty()){
-            auto p=pq.top();
+            auto p=pq.top();    
             pq.pop();
             
             int weight=p.first;
             int node=p.second;
             
-            if(inMST[node]==true){
-                continue;
-            }
+            if(inMST[node]==true) continue; // If the node is already in MST, skip it
             
+            // Include the node in MST and add the edge weight
             inMST[node]=true;
-            sum+=weight;
+            totalWeight +=weight;
             
-            for(auto &temp:adj[node]){
-                int neighbour=temp[0];
-                int neighbour_weight=temp[1];
+            
+              // Check all adjacent nodes of the current node
+            for(auto &edge:adj[node]){
+                int neighborNode =edge[0];
+                int neighborWeight =edge[1];
                 
-                if(inMST[neighbour]==false){
-                    pq.push({neighbour_weight,neighbour});
+                // If adjacent node is not in MST, push it to the heap
+                if(inMST[neighborNode]==false){
+                    pq.push({neighborWeight ,neighborNode});
                 }
             }
         }
-        return sum;
+        
+        return totalWeight ;
         
     }
 };
