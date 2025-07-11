d8ceb15bd0bb71fe2b3406da12d1b99d0f3b3e39
SAGAR KUMAR DAS <108587278+Sagarkumardas2002@users.noreply.github.com>
2025-07-11 23:57:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Trail of ones/trail-of-ones.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int countConsec(int n) {
+         if (n == 0) return 0;
+
+    vector<int> a(n + 1), b(n + 1);
+    a[1] = 1;
+    b[1] = 1;
+
+    for (int i = 2; i <= n; ++i) {
+        a[i] = a[i - 1] + b[i - 1];
+        b[i] = a[i - 1];
+    }
+
+    int total = 1 << n; // 2^n
+    int withoutConsecutive = a[n] + b[n];
+
+    return total - withoutConsecutive;
+        
+    }
+};
