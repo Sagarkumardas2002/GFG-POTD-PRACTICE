aa7b90e387c1ba1acbc6d9dd4de1bb9cbb889945
SAGAR KUMAR DAS <108587278+Sagarkumardas2002@users.noreply.github.com>
2025-12-16 15:32:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Strings Rotations of Each Other/strings-rotations-of-each-other.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    bool areRotations(string &s1, string &s2) {
+        // Step 1: Length check
+        if (s1.length() != s2.length())
+            return false;
+        
+        // Step 2: Concatenate s1 with itself
+        string temp = s1 + s1;
+        
+        // Step 3: Check if s2 is substring
+        return (temp.find(s2) != string::npos);
+    }
+};
