fa790391a725dbf79eca46f21cd9f194492cbd70
SAGAR KUMAR DAS <108587278+Sagarkumardas2002@users.noreply.github.com>
2025-12-30 15:22:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add Number Linked Lists/add-number-linked-lists.cpp
@@ -0,0 +1,66 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    // Reverse a linked list
+    Node* reverse(Node* head) {
+        Node* prev = NULL;
+        Node* curr = head;
+        
+        while (curr) {
+            Node* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+
+    Node* addTwoLists(Node* head1, Node* head2) {
+        // Step 1: Reverse both lists
+        head1 = reverse(head1);
+        head2 = reverse(head2);
+
+        Node* dummy = new Node(0);
+        Node* curr = dummy;
+        int carry = 0;
+
+        // Step 2: Add digits
+        while (head1 || head2 || carry) {
+            int sum = carry;
+
+            if (head1) {
+                sum += head1->data;
+                head1 = head1->next;
+            }
+            if (head2) {
+                sum += head2->data;
+                head2 = head2->next;
+            }
+
+            carry = sum / 10;
+            curr->next = new Node(sum % 10);
+            curr = curr->next;
+        }
+
+        // Step 3: Reverse result
+        Node* result = reverse(dummy->next);
+
+        // Step 4: Remove leading zeros
+        while (result && result->data == 0 && result->next) {
+            result = result->next;
+        }
+
+        return result;
+    }
+};
