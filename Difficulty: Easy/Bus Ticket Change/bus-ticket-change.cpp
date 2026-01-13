f14938d119482298384b55a0b686db96414443f5
SAGAR KUMAR DAS <108587278+Sagarkumardas2002@users.noreply.github.com>
2026-01-13 16:22:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bus Ticket Change/bus-ticket-change.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  public:
+    bool canServe(vector<int> &arr) {
+        int five = 0, ten = 0;
+
+        for (int note : arr) {
+            if (note == 5) {
+                five++;
+            }
+            else if (note == 10) {
+                if (five == 0) return false;
+                five--;
+                ten++;
+            }
+            else { // note == 20
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                }
+                else if (five >= 3) {
+                    five -= 3;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
