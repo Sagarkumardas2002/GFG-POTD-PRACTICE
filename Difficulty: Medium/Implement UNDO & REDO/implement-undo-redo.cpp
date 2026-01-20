81d3f811dee6940f562893063101e3c29589f748
SAGAR KUMAR DAS <108587278+Sagarkumardas2002@users.noreply.github.com>
2026-01-20 14:20:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Implement UNDO & REDO/implement-undo-redo.cpp
@@ -0,0 +1,36 @@
+class Solution {
+private:
+    string doc;
+    stack<char> undoStack;
+    stack<char> redoStack;
+
+public:
+    void append(char x) {
+        doc.push_back(x);
+        undoStack.push(x);
+        while (!redoStack.empty())
+            redoStack.pop(); // clear redo history
+    }
+
+    void undo() {
+        if (!undoStack.empty()) {
+            char last = undoStack.top();
+            undoStack.pop();
+            doc.pop_back();
+            redoStack.push(last);
+        }
+    }
+
+    void redo() {
+        if (!redoStack.empty()) {
+            char last = redoStack.top();
+            redoStack.pop();
+            doc.push_back(last);
+            undoStack.push(last);
+        }
+    }
+
+    string read() {
+        return doc;
+    }
+};
