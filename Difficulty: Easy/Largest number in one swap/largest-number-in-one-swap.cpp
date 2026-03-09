0c67b1c33ac1938e126b446491726c49e906f438
SAGAR KUMAR DAS <108587278+Sagarkumardas2002@users.noreply.github.com>
2026-03-09 17:32:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Largest number in one swap/largest-number-in-one-swap.cpp
+++ Difficulty: Easy/Largest number in one swap/largest-number-in-one-swap.cpp
@@ -1,27 +1,25 @@
 class Solution {
   public:
-    string largestSwap(string s) {
+    string largestSwap(string &s) {
         int n = s.size();
-        vector<int> maxRight(n);
-
-        // maxRight[i] stores the index of the rightmost largest digit from i..n-1
-        maxRight[n - 1] = n - 1;
-        for (int i = n - 2; i >= 0; i--) {
-            int rightMaxInd = maxRight[i + 1];
-            char rightMaxElement = s[rightMaxInd];
-            // strictly '>' so that we keep the rightmost max
-            maxRight[i] = (s[i] > rightMaxElement) ? i : rightMaxInd;
+        
+        vector<int> last(10, -1);
+        
+        // store last occurrence of each digit
+        for(int i = 0; i < n; i++){
+            last[s[i] - '0'] = i;
         }
-
-        // find the first place where a swap can make string larger
-        for (int i = 0; i < n; i++) {
-            int maxRightIndex = maxRight[i];
-            if (s[i] < s[maxRightIndex]) {
-                swap(s[i], s[maxRightIndex]);
-                return s;  // return immediately after swap
+        
+        for(int i = 0; i < n; i++){
+            // check for larger digit
+            for(int d = 9; d > s[i] - '0'; d--){
+                if(last[d] > i){
+                    swap(s[i], s[last[d]]);
+                    return s;
+                }
             }
         }
-
-        return s; // if no swap improves result
+        
+        return s;
     }
 };
