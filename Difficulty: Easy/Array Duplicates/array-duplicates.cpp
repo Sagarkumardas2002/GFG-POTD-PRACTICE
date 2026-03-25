283b61e35dccc17c0e7dd332ef34eca0f8abc0b2
SAGAR KUMAR DAS <108587278+Sagarkumardas2002@users.noreply.github.com>
2026-03-25 18:41:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Duplicates/array-duplicates.cpp
+++ Difficulty: Easy/Array Duplicates/array-duplicates.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& arr) {
-        vector<int> ans;
-
-        for(int i = 0; i < arr.size(); i++) {
-            int val = abs(arr[i]);
-            int idx = val - 1;
-
-            // If already negative, number seen before → duplicate
-            if(arr[idx] < 0) {
-                ans.push_back(val);
-            } 
-            else {
-                arr[idx] = -arr[idx]; // Mark visited
-            }
-        }
-
-        return ans;
+       int n=arr.size();
+       vector<int>result;
+       
+       unordered_map<int,int>mp;
+       
+       for(auto it:arr){
+           mp[it]++;
+       }
+       
+      
+      for(auto it:mp){
+          if(it.second>1){
+              result.push_back(it.first);
+          }
+      }
+       return result;
     }
 };
