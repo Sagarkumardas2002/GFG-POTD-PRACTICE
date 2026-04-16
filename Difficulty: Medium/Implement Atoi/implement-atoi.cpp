e3a08a2168cf8f622a55d20c5ebc20e7eea1e824
SAGAR KUMAR DAS <108587278+Sagarkumardas2002@users.noreply.github.com>
2026-04-16 13:45:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Implement Atoi/implement-atoi.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    int myAtoi(string &s) {
+        int i = 0, n = s.size();
+
+        // 1. Skip spaces
+        while (i < n && s[i] == ' ') i++;
+
+        // 2. Check sign
+        int sign = 1;
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-') sign = -1;
+            i++;
+        }
+
+        // 3. Read digits
+        long long num = 0;
+        while (i <=n && isdigit(s[i])) {
+            num = num * 10 + (s[i] - '0');
+            i++;
+        }
+
+        num *= sign;
+
+        // 4. Handle overflow
+        if (num > INT_MAX) return INT_MAX;
+        if (num < INT_MIN) return INT_MIN;
+
+        return (int)num;
+    }
+};
