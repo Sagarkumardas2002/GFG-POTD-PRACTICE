a1df87021a6c9ba7f0fcdd8bf438b62788aa03f2
SAGAR KUMAR DAS <108587278+Sagarkumardas2002@users.noreply.github.com>
2026-06-28 19:08:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/k Times Appearing Adjacent Two 1's/k-times-appearing-adjacent-two-1s.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+    int countStrings(int n, int k) {
+        const int MOD = 1000000007;
+
+        vector<vector<vector<long long>>> dp(
+            n + 1,
+            vector<vector<long long>>(k + 1, vector<long long>(2, 0))
+        );
+
+        dp[1][0][0] = 1;
+        dp[1][0][1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j <= k; j++) {
+
+                // End with 0
+                dp[i][j][0] =
+                    (dp[i - 1][j][0] + dp[i - 1][j][1]) % MOD;
+
+                // End with 1 after 0
+                dp[i][j][1] =
+                    (dp[i][j][1] + dp[i - 1][j][0]) % MOD;
+
+                // End with 1 after 1
+                if (j > 0) {
+                    dp[i][j][1] =
+                        (dp[i][j][1] + dp[i - 1][j - 1][1]) % MOD;
+                }
+            }
+        }
+
+        return (dp[n][k][0] + dp[n][k][1]) % MOD;
+    }
+};
