3c8f0ba41e0dac09a5830fb1fc0668695bfb6278
SAGAR KUMAR DAS <108587278+Sagarkumardas2002@users.noreply.github.com>
2026-07-19 18:46:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Mountain Subarray Queries/mountain-subarray-queries.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<bool> processQueries(vector<int> &arr, vector<vector<int>> &queries) {
+        int n = arr.size();
+
+        vector<int> inc(n), dec(n);
+
+        // inc[i] = farthest index reachable by non-decreasing moves
+        inc[n - 1] = n - 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] <= arr[i + 1])
+                inc[i] = inc[i + 1];
+            else
+                inc[i] = i;
+        }
+
+        // dec[i] = farthest left index of non-increasing segment
+        dec[0] = 0;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] <= arr[i - 1])
+                dec[i] = dec[i - 1];
+            else
+                dec[i] = i;
+        }
+
+        vector<bool> ans;
+
+        for (auto &q : queries) {
+            int l = q[0];
+            int r = q[1];
+
+            ans.push_back(dec[r] <= inc[l]);
+        }
+
+        return ans;
+    }
+};
